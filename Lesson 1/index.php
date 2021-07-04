757505bf983d19cbd9152842c18ec7dd8c3c0a29
Alexandra Matievskaya <83234836+AleksGesse@users.noreply.github.com>
2021-07-05 02:07:29 +0300

    Add files via upload

---
+++ Lesson 1/index.php
@@ -0,0 +1,71 @@
+<?php
+
+// Task 1: print 5 variables with echo:
+$a = 'Alexandra';
+
+echo $a;
+echo ' <br/>';
+
+$b = 54;
+echo ((bool) $b);
+echo ' <br/>';
+
+$c = 26;
+echo $c;
+echo ' <br/>';
+
+$d = 26.5;
+echo $d;
+echo ' <br/>';
+
+$e = null;
+echo $e, 'here is null';
+echo ' <br/>';
+
+// Task 2: print arithmetic operators with var_dump:
+
+var_dump($d - $c);
+echo ' <br/>';
+
+var_dump($d + $c);
+echo ' <br/>';
+
+var_dump($d * $c);
+echo ' <br/>';
+
+var_dump($d / $c);
+echo ' <br/>';
+
+var_dump($d % $c);
+echo ' <br/>';
+
+var_dump($d ** $c);
+echo ' <br/>';
+
+// Task 3: print comparison operators with var_dump:
+
+var_dump($d == $c);
+echo ' <br/>';
+
+var_dump($d != $c);
+echo ' <br/>';
+
+var_dump($d < $c);
+echo ' <br/>';
+
+var_dump($d > $c);
+echo ' <br/>';
+
+var_dump($d <= $c);
+echo ' <br/>';
+
+var_dump($d >= $c);
+echo ' <br/>';
+
+var_dump($d === $c);
+echo ' <br/>';
+
+var_dump($d !== $c);
+echo ' <br/>';
+
+
